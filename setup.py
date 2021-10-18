6fd19dc667b424a3841bc77f4d51045a9a130ce0
AnonymouX47 <feyidab01@gmail.com>
2021-10-18 17:24:49 +0100

    Updated setup script

--- setup.py
+++ setup.py
@@ -7,26 +7,39 @@ classifiers = [
     "License :: OSI Approved :: MIT License",
     "Topic :: Software Development :: Libraries",
     "Topic :: Utilities",
+    "Topic :: Scientific/Engineering :: Image Processing",
 ]
 
 with open("README.md", "r") as fp:
     long_description = fp.read()
 
 setup(
-    name="terminal-img",
-    version="0.0.1",
-    author="Pranav Baburaj",
-    author_email="i.am.pranav.baburaj@gmail.com",
-    url="https://github.com/pranavbaburaj/img",
-    py_modules=["image", "cli"],
+    name="term-img",
+    version="0.0.3",
+    author="Anonymoux47",
+    author_email="lekzy771@gmail.com",
+    url="https://github.com/AnonymouX47/img",
     description="Display images in the terminal",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=["term_img"],
     license="MIT",
     classifiers=classifiers,
     python_requires=">=3.6",
     install_requires=["pillow", "requests"],
     entry_points={
-        "console_scripts": ["img=cli:main"],
+        "console_scripts": ["term-img=term_img.cli:main"],
     },
+    keywords=[
+        "image",
+        "terminal",
+        "viewer",
+        "PIL",
+        "Pillow",
+        "console",
+        "xterm",
+        "library",
+        "cli",
+        "tui",
+    ],
 )
