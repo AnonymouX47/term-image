38fba4f668deb8863448a59369552fd10ece1401
AnonymouX47 <anonymoux47@gmail.com>
2021-12-31 16:42:12 +0100

    Updated exit codes

--- term_img/exit_codes.py
+++ term_img/exit_codes.py
@@ -3,10 +3,11 @@
 codes = {
     0: "SUCCESS",
     1: "FAILURE",
-    2: "NO_VALID_SOURCE",
-    3: "INVALID_SIZE",
-    4: "CONFIG_ERROR",
-    5: "INTERRUPTED",
+    # Omit bash shell exit code 2
+    3: "NO_VALID_SOURCE",
+    4: "INVALID_SIZE",
+    5: "CONFIG_ERROR",
+    6: "INTERRUPTED",
 }
 
 globals().update(((v, k) for k, v in codes.items()))
