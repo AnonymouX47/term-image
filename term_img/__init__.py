8be6fbffaad9f957cd1c461428c1c81ecb49d3e6
AnonymouX47 <anonymoux47@gmail.com>
2022-03-08 23:00:37 +0100

    Bumped package version to 0.2.0.dev0

--- term_img/__init__.py
+++ term_img/__init__.py
@@ -19,7 +19,7 @@ Copyright (c) 2022
 
 __all__ = ("set_font_ratio", "get_font_ratio")
 
-version_info = (0, 1, 1)
+version_info = (0, 2, 0, "dev0")
 __version__ = ".".join(map(str, version_info))
 
 
