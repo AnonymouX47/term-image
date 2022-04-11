AnonymouX47/term-image

commit 9de3af36a0987b3b89641366abdaf052dd1a4955

[tui] Implemented asynchronous image rendering
- Add: Added `manage_image_renders()` and `image_render_queue` in `.tui.render`.
- Add: Added the ImageRenderManager thread executing `.tui.render.manage_image_renders()`.
  - Updated `.tui.init()`.
- Change: Heavily updated `.tui.widgets.Image`, particularly it's `render()` method.
- Change: Removed redundant code.

term_img/tui/render.py
@@ -11,6 +11,72 @@ from .. import get_font_ratio, logging, notify, set_font_ratio
 from ..logging_multi import redirect_logs
 
 
+def manage_image_renders():
+    from .widgets import Image, ImageCanvas, image_box
+
+    multi = logging.MULTI
+    image_render_in = (mp_Queue if multi else Queue)()
+    image_render_out = (mp_Queue if multi else Queue)()
+    renderer = (Process if multi else Thread)(
+        target=render_images,
+        args=(
+            image_render_in,
+            image_render_out,
+            get_font_ratio(),
+            multi,
+            False,
+        ),
+        name="ImageRenderer",
+    )
+    renderer.start()
+    faulty_image = Image._faulty_image
+    last_image_w = image_box.original_widget
+    # To prevent an `AttributeError` with the first deletion, while avoiding `hasattr()`
+    last_image_w._canv = None
+
+    while True:
+        image_w, size, alpha = image_render_queue.get()
+        if not image_w:
+            break
+        if image_w is not image_box.original_widget:
+            continue
+
+        # Stored at this point to prevent an incorrect *rendered_size* for the
+        # Imagecanvas, since the image's size might've changed by the time the canvas is
+        # being created.
+        rendered_size = image_w._image.rendered_size
+
+        Image._rendering_image_info = (image_w, size, alpha)
+        image_render_in.put(
+            (image_w._image._source if multi else image_w._image, size, alpha)
+        )
+        notify.start_loading()
+        render = image_render_out.get()
+        Image._rendering_image_info = (None,) * 3
+
+        if image_w is image_box.original_widget:
+            del last_image_w._canv
+            if render:
+                image_w._canv = ImageCanvas(
+                    render.encode().split(b"\n"), size, rendered_size
+                )
+            else:
+                image_w._canv = faulty_image.render(size)
+                if not image_w._faulty:
+                    # Ensure a fault is logged only once per image, per directory scan
+                    image_w._faulty = True
+                    logging.log_exception(
+                        f"{image_w._image._source!r} could not be loaded",
+                        logger,
+                        direct=True,
+                    )
+            last_image_w = image_w
+        notify.stop_loading()
+
+    image_render_in.put((None,) * 3)
+    renderer.join()
+
+
 def manage_grid_renders(n_renderers: int):
     """Manages grid cell rendering.
 
@@ -182,3 +248,4 @@ def render_images(
 
 logger = _logging.getLogger(__name__)
 grid_render_queue = Queue()
+image_render_queue = Queue()

term_img/tui/__init__.py
@@ -12,7 +12,7 @@ import urwid
 from .. import logging
 from . import main
 from .main import process_input, scan_dir_grid, scan_dir_menu, sort_key_lexi
-from .render import manage_grid_renders
+from .render import image_render_queue, manage_grid_renders, manage_image_renders
 from .widgets import Image, info_bar, main as main_widget
 
 
@@ -52,6 +52,11 @@ def init(
         name="GridRenderManager",
         daemon=True,
     )
+    image_render_manager = Thread(
+        target=manage_image_renders,
+        name="ImageRenderManager",
+        daemon=True,
+    )
 
     main.loop.screen.clear()
     main.loop.screen.set_terminal_properties(2 ** 24)
@@ -67,13 +72,15 @@ def init(
     menu_scanner.start()
     grid_scanner.start()
     grid_render_manager.start()
+    image_render_manager.start()
 
     try:
         print("\033[?1049h", end="", flush=True)  # Switch to the alternate buffer
         next(main.displayer)
         main.loop.run()
-        main.grid_active.set()  # Allow GridRenderManager to receive quitting signal
         grid_render_manager.join()
+        image_render_queue.put((None,) * 3)
+        image_render_manager.join()
         logging.log("Exited TUI normally", logger, direct=False)
     except (KeyboardInterrupt, Exception):
         main.interrupted.set()  # Signal interruption to other threads.

term_img/tui/widgets.py
@@ -13,7 +13,7 @@ from .. import logging
 from ..config import _nav, cell_width, expand_key, nav
 from ..image import _ALPHA_THRESHOLD, TermImage
 from . import keys, main as tui_main
-from .render import grid_render_queue
+from .render import grid_render_queue, image_render_queue
 
 command = urwid.Widget._command_map._command_defaults.copy()
 for action, (key, _) in _nav.items():
@@ -135,7 +135,10 @@ class Image(urwid.Widget):
 
     _frame = _frame_changed = _frame_size_hash = None
 
-    _last_canv = (None, None)
+    _faulty = False
+    _canv = None
+    _rendering_image_info = (None,) * 3
+
     _grid_cache = {}
 
     _alpha = f"{_ALPHA_THRESHOLD}"[1:]  # Updated from `.tui.init()`
@@ -161,23 +164,13 @@ class Image(urwid.Widget):
         context = tui_main.get_context()
         image = self._image
 
-        # Cache retrieval
-
-        if (
-            (not image._is_animated or tui_main.NO_ANIMATION)
-            and (
-                view.original_widget is not image_grid_box
-                or context == "full-grid-image"
-            )
-            and self._last_canv[0] == hash((image._source, size))
-        ):
-            if context in self._force_render_contexts:
-                keys.disable_actions(context, "Force Render")
-            return self._last_canv[1]
-
         # Forced render
 
-        if mul(*image._original_size) > tui_main.MAX_PIXELS:
+        if mul(*image._original_size) > tui_main.MAX_PIXELS and not (
+            self._canv
+            and self._canv.size == size
+            or (self, size, self._alpha) == __class__._rendering_image_info
+        ):
             if self._force_render:
                 # `.main.animate_image()` deletes `_force_render` when done with an
                 # image to avoid the cost of attribute creation and deletion per frame
@@ -195,6 +188,7 @@ class Image(urwid.Widget):
                 if context in self._force_render_contexts:
                     keys.enable_actions(context, "Force Render")
                 return __class__._large_image.render(size, focus)
+
         if context in self._force_render_contexts:
             keys.disable_actions(context, "Force Render")
 
@@ -239,49 +233,35 @@ class Image(urwid.Widget):
 
         # Rendering
 
-        try:
-            if hasattr(self, "_animator"):
-                if self._frame_changed:
-                    self._frame = next(self._animator)
-                    self._frame_changed = False
-                    self._frame_size_hash = hash(size)
-                elif hash(size) != self._frame_size_hash:
-                    # If size changed, re-render the current frame the usual way,
-                    # with the new size
-                    self._frame = f"{image:1.1{self._alpha}}"
-                    self._frame_size_hash = hash(size)
-
-            # Using `TermImage` for padding will use more memory since all the
-            # spaces will be in the render output string, and theoretically more time
-            # with all the checks and string splitting & joining.
-            # While `ImageCanvas` is better since it only stores the main image render
-            # string (as a list though) then generates and yields the complete lines
-            # **as needed**. Trimmed padding lines are never generated at all.
+        if hasattr(self, "_animator"):
+            if self._frame_changed:
+                self._frame = next(self._animator)
+                self._frame_changed = False
+                self._frame_size_hash = hash(size)
+            elif hash(size) != self._frame_size_hash:
+                # If size changed, re-render the current frame the usual way,
+                # with the new size
+                self._frame = f"{image:1.1{self._alpha}}"
+                self._frame_size_hash = hash(size)
             canv = ImageCanvas(
-                (self._frame or f"{image:1.1{self._alpha}}").encode().split(b"\n"),
-                size,
-                image.rendered_size,
+                self._frame.encode().split(b"\n"), size, image.rendered_size
             )
-        except Exception:
-            if not hasattr(self, "_faulty"):
-                # Ensure a fault is logged only once per image, per directory scan
-                self._faulty = None
-                logging.log_exception(
-                    f"{image._source!r} could not be loaded",
-                    logger,
-                    direct=(
-                        view.original_widget is image_box
-                        or context == "full-grid-image"
-                    ),
+        elif view.original_widget is image_grid_box and context != "full-grid-image":
+            # When the grid render cell width adjusts; when _maxcols_ < _cell_width_
+            try:
+                canv = ImageCanvas(
+                    f"{image:1.1{self._alpha}}".encode().split(b"\n"),
+                    size,
+                    image.rendered_size,
                 )
-            canv = __class__._faulty_image.render(size, focus)
-
-        # Cache storing
-
-        if (not image._is_animated or tui_main.NO_ANIMATION) and (
-            view.original_widget is not image_grid_box or context == "full-grid-image"
-        ):
-            __class__._last_canv = (hash((image._source, size)), canv)
+            except Exception:
+                canv = __class__._faulty_image.render(size, focus)
+        elif self._canv and self._canv.size == size:
+            canv = self._canv
+        else:
+            if (self, size, self._alpha) != __class__._rendering_image_info:
+                image_render_queue.put((self, size, self._alpha))
+            canv = __class__._placeholder.render(size)
 
         return canv
 

commit ec4e8b2e0fb606c256c1573cf456575ff59e9067

[tui] Fixed logging of loading/rendering errors
- Fix: Restored logging of errors raised while loading or rendering an image.
  - Broken by 9de3af36a0987b3b89641366abdaf052dd1a4955 in #15.
- Add: Added *log_faults* parameter to `.tui.render.render_images()`.
- Change: Changed *multi* and *out_extras* parameter of `.tui.render.render_images()` to be keyword-only.

term_img/tui/render.py
@@ -20,13 +20,8 @@ def manage_image_renders():
     image_render_out = (mp_Queue if multi else Queue)()
     renderer = (Process if multi else Thread)(
         target=render_images,
-        args=(
-            image_render_in,
-            image_render_out,
-            get_font_ratio(),
-            multi,
-            False,
-        ),
+        args=(image_render_in, image_render_out, get_font_ratio()),
+        kwargs=dict(multi=multi, out_extras=False, log_faults=True),
         name="ImageRenderer",
     )
     renderer.start()
@@ -49,7 +44,12 @@ def manage_image_renders():
 
         Image._rendering_image_info = (image_w, size, alpha)
         image_render_in.put(
-            (image_w._image._source if multi else image_w._image, size, alpha)
+            (
+                image_w._image._source if multi else image_w._image,
+                size,
+                alpha,
+                image_w._faulty,
+            )
         )
         notify.start_loading()
         render = image_render_out.get()
@@ -63,19 +63,14 @@ def manage_image_renders():
                 )
             else:
                 image_w._canv = faulty_image.render(size)
+                # Ensures a fault is logged only once per `Image` instance
                 if not image_w._faulty:
-                    # Ensure a fault is logged only once per image, per directory scan
                     image_w._faulty = True
-                    logging.log_exception(
-                        f"{image_w._image._source!r} could not be loaded",
-                        logger,
-                        direct=True,
-                    )
             update_screen()
             last_image_w = image_w
         notify.stop_loading()
 
-    image_render_in.put((None,) * 3)
+    image_render_in.put((None,) * 4)
     renderer.join()
 
 
@@ -102,9 +97,8 @@ def manage_grid_renders(n_renderers: int):
                 grid_render_in,
                 grid_render_out,
                 get_font_ratio(),
-                multi,
-                True,
             ),
+            kwargs=dict(multi=multi, out_extras=True, log_faults=False),
             name="GridRenderer" + f"-{n}" * multi,
         )
         for n in range(n_renderers if multi else 1)
@@ -193,8 +187,10 @@ def render_images(
     input: Union[Queue, mp_Queue],
     output: Union[Queue, mp_Queue],
     font_ratio: float,
+    *,
     multi: bool,
     out_extras: bool,
+    log_faults: bool,
 ):
     """Renders images.
 
@@ -212,7 +208,10 @@ def render_images(
     logger.debug("Starting")
     try:
         while True:
-            image, size, alpha = input.get()
+            if log_faults:
+                image, size, alpha, faulty = input.get()
+            else:
+                image, size, alpha = input.get()
             if not image:  # Quitting
                 break
 
@@ -238,6 +237,13 @@ def render_images(
                     if out_extras
                     else None
                 )
+                # *faulty* ensures a fault is logged only once per `Image` instance
+                if log_faults and not faulty:
+                    logging.log_exception(
+                        f"Failed to load or render {image._source!r}",
+                        logger,
+                        direct=True,
+                    )
     except KeyboardInterrupt:
         # Logging here could potentially result in an exception if MultiLogger has ended
         # and the associated manager process has shutdown.