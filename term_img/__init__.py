c27ccb1a907a1776093aeb2b4ec00f3e669a4ec6
AnonymouX47 <anonymoux47@gmail.com>
2022-04-20 23:57:45 +0100

    [lib] Added `DeprecationWarning` to `term_img`
    - Add: The package now issues a `DeprecationWarning` when imported with `term_img`.

--- term_img/__init__.py
+++ term_img/__init__.py
@@ -1,4 +1,13 @@
 """Backwards-compatibility dummy for `term_image`"""
 
+import warnings
+
 from term_image import *
 from term_image import __all__, __author__, __version__, version_info
+
+warnings.filterwarnings("once", category=DeprecationWarning, module="term_img")
+warnings.warn(
+    "The top-level package has been renamed to `term_image` and will no longer "
+    "be accessible via the name `term_img` as from version 1.0.0",
+    DeprecationWarning,
+)
