f5d9d73ef34f33ecff50bbfdbc0adc5ddbd2c684
AnonymouX47 <anonymoux47@gmail.com>
2022-05-04 16:49:16 +0100

    [lib] Added `.utils.no_redecorate()`
    - Add: Added `.utils.no_redecorate()` decorator function.
    - Change: Replaced boilerplate code in `.image.common._close_validate()`.
    - Change: Renamed `.utils._color()` to `.utils.color()`.

--- term_image/utils.py
+++ term_image/utils.py
@@ -1,13 +1,37 @@
 from __future__ import annotations
 
-from typing import Tuple
+__all__ = (
+    "no_redecorate",
+    "color",
+)
 
-_BG_FMT = "\033[48;2;%d;%d;%dm"
-_FG_FMT = "\033[38;2;%d;%d;%dm"
-_RESET = "\033[0m"
+from functools import wraps
+from types import FunctionType
+from typing import Callable
+
+# Decorators
 
 
-def _color(
+def no_redecorate(decor: Callable) -> FunctionType:
+    """Decorates a decorator to prevent it from re-decorating objects."""
+    if getattr(decor, "_no_redecorate_", False):
+        return decor
+
+    @wraps(decor)
+    def no_redecorate_wrapper(obj, *args, **kwargs):
+        if not getattr(obj, f"_{decor.__name__}_", False):
+            obj = decor(obj, *args, **kwargs)
+            setattr(obj, f"_{decor.__name__}_", True)
+        return obj
+
+    no_redecorate_wrapper._no_redecorate_ = True
+    return no_redecorate_wrapper
+
+
+# Non-decorators
+
+
+def color(
     text: str, fg: Tuple[int] = (), bg: Tuple[int] = (), *, end: bool = False
 ) -> str:
     """Prepends *text* with 24-bit color escape codes for the given foreground and/or
@@ -20,3 +44,8 @@ def _color(
         *bg,
         text,
     ) + _RESET * end
+
+
+_BG_FMT = "\033[48;2;%d;%d;%dm"
+_FG_FMT = "\033[38;2;%d;%d;%dm"
+_RESET = "\033[0m"
