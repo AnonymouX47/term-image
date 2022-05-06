cca256a7c3479255d436fe03e3e37ad49adf60d5
AnonymouX47 <anonymoux47@gmail.com>
2022-05-06 19:18:04 +0100

    [lib] Modelled the kitty graphics control data
    - Add: Added `.image.kitty.ControlData`.
    - Add: Defined related constants in `.image.kitty`.

--- term_image/image/kitty.py
+++ term_image/image/kitty.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 __all__ = ("KittyImage",)
 
+from dataclasses import dataclass
 from math import ceil
 from operator import mul
 from typing import Optional, Tuple
@@ -68,3 +69,89 @@ class KittyImage(BaseImage):
             if pixels is not None
             else lines * (get_cell_size() or (1, 2))[1]
         )
+
+
+# Values for control data keys with limited set of values
+
+
+class a:
+    TRANS = "t"
+    TRANS_DISP = "T"
+    QUERY = "q"
+    PLACE = "p"
+    DELETE = "d"
+    TRANS_FRAMES = "f"
+    CONTROL_ANIM = "a"
+    COMPOSE_FRAMES = "c"
+
+
+class C:
+    MOVE = 0
+    STAY = 1
+
+
+class f:
+    RGB = 24
+    RGBA = 32
+    PNG = 100
+
+
+class o:
+    ZLIB = "z"
+
+
+class t:
+    DIRECT = "d"
+    FILE = "f"
+    TEMP = "t"
+    SHARED = "s"
+
+
+class z:
+    BEHIND = -1
+    IN_FRONT = 0
+
+
+@dataclass
+class ControlData:
+    """Represents a portion of the kitty terminal graphics protocol control data"""
+
+    a: Optional[str] = a.TRANS_DISP  # action
+    f: Optional[int] = f.RGBA  # data format
+    t: Optional[str] = t.DIRECT  # transmission medium
+    s: Optional[int] = None  # image width
+    v: Optional[int] = None  # image height
+    z: Optional[int] = z.IN_FRONT  # z-index
+    o: Optional[str] = o.ZLIB  # compression
+
+    # # Image display size in columns and rows/lines
+    # # The image is shrunk or enlarged to fit
+    c: Optional[int] = None  # columns
+    r: Optional[int] = None  # rows
+
+    def __post_init__(self):
+        if self.f == f.PNG:
+            self.s = self.v = None
+
+
+class _ControlData:  # Currently Unused
+
+    i: Optional[int] = None  # image ID
+    d: Optional[str] = None  # delete images
+    m: Optional[int] = None  # payload chunk
+    C: Optional[int] = C.STAY  # cursor movement policy
+    O: Optional[int] = None  # data start offset; with t=s or t=f
+    S: Optional[int] = None  # data size in bytes; with f=100,o=z or t=s or t=f
+
+    # Origin offset (px) within the current cell; Must be less than the cell size
+    # (0, 0) == Top-left corner of the cell; Not used with `c` and `r`.
+    X: Optional[int] = None
+    Y: Optional[int] = None
+
+    # Image crop (px)
+    # # crop origin; (0, 0) == top-left corner of the image
+    x: Optional[int] = None
+    y: Optional[int] = None
+    # # crop rectangle size
+    w: Optional[int] = None
+    h: Optional[int] = None
