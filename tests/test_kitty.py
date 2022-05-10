fae08ff90939aa49b6ba4dcc9e50ad293a8973b9
AnonymouX47 <anonymoux47@gmail.com>
2022-05-10 16:59:27 +0100

    [test] Added tests for render method selection

--- tests/test_kitty.py
+++ tests/test_kitty.py
@@ -2,12 +2,49 @@
 
 import pytest
 
-from term_image.image import KittyImage
+from term_image.image.kitty import LINES, WHOLE, KittyImage
 
 from .common import *  # noqa:F401
-from .common import setup_common
+from .common import python_img, setup_common
 
 
 @pytest.mark.order("first")
 def test_setup_common():
     setup_common(KittyImage)
+
+
+def test_set_render_method():
+    try:
+        assert KittyImage._render_method == KittyImage._default_render_method == LINES
+        image = KittyImage(python_img)
+        assert image._render_method == KittyImage._default_render_method
+
+        assert KittyImage.set_render_method(WHOLE) is True
+        assert KittyImage._render_method == WHOLE
+        assert image._render_method == WHOLE
+
+        assert image.set_render_method(LINES) is True
+        assert image._render_method == LINES
+
+        assert image.set_render_method() is True
+        assert image._render_method == WHOLE
+
+        assert KittyImage.set_render_method(LINES) is True
+        assert KittyImage._render_method == LINES
+        assert image._render_method == LINES
+
+        assert image.set_render_method(WHOLE) is True
+        assert image._render_method == WHOLE
+
+        assert image.set_render_method() is True
+        assert image._render_method == LINES
+
+        assert KittyImage.set_render_method(WHOLE) is True
+        assert KittyImage._render_method == WHOLE
+        assert image._render_method == WHOLE
+
+        assert KittyImage.set_render_method() is True
+        assert KittyImage._render_method == KittyImage._default_render_method
+        assert image._render_method == KittyImage._default_render_method
+    finally:
+        KittyImage._render_method = KittyImage._default_render_method
