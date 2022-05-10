AnonymouX47/term-image

commit 49e1430387a87457c894a4000b9d52493f71f3d1

[lib] Restricted ``KittyImage`` to Kitty >= 0.20.0

term_image/image/kitty.py
@@ -8,6 +8,7 @@ from base64 import standard_b64encode
 from dataclasses import asdict, dataclass
 from math import ceil
 from operator import mul
+from subprocess import run
 from typing import Generator, Optional, Tuple, Union
 from zlib import compress, decompress
 
@@ -26,6 +27,9 @@ class KittyImage(BaseImage):
           support the protocol.
 
     See :py:class:`BaseImage` for the complete description of the constructor.
+
+    ATTENTION:
+        Requires `Kitty <https://sw.kovidgoyal.net/kitty/>`_ >= 0.20.0.
     """
 
     _pixel_ratio = 1.0  # Size unit conversion already involves cell size calculation
@@ -50,6 +54,24 @@ class KittyImage(BaseImage):
             # Not supported if it doesn't respond to either query
             # or responds to the second but not the first
             cls._supported = bool(response and response.rpartition(b"\033")[0])
+
+            # Currently, only kitty >= 0.20.0 implements the protocol features utilized
+            if cls._supported:
+                result = run(
+                    "kitty +kitten query-terminal --wait-for=0.1 name version",
+                    shell=True,
+                    text=True,
+                    capture_output=True,
+                )
+                name, version = map(
+                    lambda query: query.partition(" ")[2], result.stdout.split("\n", 1)
+                )
+
+                cls._supported = (
+                    not result.returncode
+                    and name == "xterm-kitty"
+                    and tuple(map(int, version.split("."))) >= (0, 20, 0)
+                )
 
         return cls._supported
 

commit 73eb7fb72fd853dbc7a282072efbd817b7df042c

[lib] Fixed `KittyImage` support check
- Fix: `KittyImage.is_supported()` is now input-locked as it involves querying the terminal (by an external process).
  - Broken by commit 49e1430.

term_image/image/kitty.py
@@ -15,7 +15,7 @@ from zlib import compress, decompress
 import PIL
 
 from ..exceptions import TermImageException
-from ..utils import get_cell_size, query_terminal
+from ..utils import get_cell_size, lock_input, query_terminal
 from .common import BaseImage
 
 
@@ -42,6 +42,7 @@ class KittyImage(BaseImage):
         super().__init__(image, **kwargs)
 
     @classmethod
+    @lock_input
     def is_supported(cls):
         if cls._supported is None:
             # Kitty graphics query + terminal attribute query