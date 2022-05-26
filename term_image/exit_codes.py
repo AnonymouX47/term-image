da6b8ac9e4b48651d3a699e33fba7068049655c8
AnonymouX47 <anonymoux47@gmail.com>
2022-05-26 22:40:42 +0100

    [cli] Changed `INVALID_ARG` exit code; `6` -> `2`

--- term_image/exit_codes.py
+++ term_image/exit_codes.py
@@ -5,11 +5,10 @@ from __future__ import annotations
 codes = {
     0: "SUCCESS",
     1: "FAILURE",
-    # Omit bash shell exit code 2
+    2: "INVALID_ARG",
     3: "INTERRUPTED",
     4: "CONFIG_ERROR",
     5: "NO_VALID_SOURCE",
-    6: "INVALID_ARG",
 }
 
 globals().update(((v, k) for k, v in codes.items()))
