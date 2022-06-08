d3d7d371614c9b85424bde8f73d35eaa0f06d12b
AnonymouX47 <anonymoux47@gmail.com>
2022-06-08 15:14:06 +0100

    [lib] Implemented `iterm2` rendering
    - Add: Implemented "lines" and "whole" render methods for `iterm2` render style.
    - Add: Added `_render_method`, `_render_methods`, `_default_render_method` and `_render_image()` to `ITerm2Image`.
    - Updated `ITerm2Image` docstring.

--- term_image/image/iterm2.py
+++ term_image/image/iterm2.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 
+import io
 import re
+from base64 import standard_b64encode
+from typing import Set, Union
+
+import PIL
 
 from ..utils import lock_tty, query_terminal, read_tty
 from .common import GraphicsImage
+
+# Constants for render methods
+LINES = "lines"
+WHOLE = "whole"
 
 
 class ITerm2Image(GraphicsImage):
@@ -11,13 +20,46 @@ class ITerm2Image(GraphicsImage):
 
     See :py:class:`GraphicsImage` for the complete description of the constructor.
 
+    **Render Methods:**
+
+    :py:class:`KittyImage` provides two methods of :term:`rendering` images, namely:
+
+    lines
+       Renders an image line-by-line i.e the image if evenly split up across
+       the number of line it should occupy and all portions is joined together by
+       ``\\n`` (newline sequence) to give the whole image.
+
+       Pros:
+
+         * Good for use cases where it might be required to trim some lines of the
+           image.
+
+    whole
+       Renders an image all at once i.e the entire image data is encoded into the first
+       line of the :term:`rendered` output, such that the entire image is drawn once
+       by the terminal and still occupies the proper amount of lines and columns.
+
+       Pros:
+
+         * Renders faster and results are more compact (i.e less in character count)
+           compared to the ``lines`` method since the entire image is encoded at once.
+         * Better for images that are large in resolution and pixel density.
+
+    The render method can be set with
+    :py:meth:`set_render_method() <BaseImage.set_render_method>` using the names
+    specified above.
+
     ATTENTION:
         Currently supported terminal emulators include:
 
-          * `ITerm2 <https://iterm2.com>`_.
+          * `iTerm2 <https://iterm2.com>`_.
           * `Konsole <https://konsole.kde.org>`_ >= 22.04.0.
           * `WezTerm <https://wezfurlong.org/wezterm/>`_.
     """
+
+    _render_methods: Set[str] = {LINES, WHOLE}
+    _default_render_method: str = LINES
+    _render_method: str = LINES
 
     _TERM: str = ""
     _TERM_VERSION: str = ""
@@ -57,3 +99,87 @@ class ITerm2Image(GraphicsImage):
                 cls._supported = False
 
         return cls._supported
+
+    def _render_image(
+        self, img: PIL.Image.Image, alpha: Union[None, float, str]
+    ) -> str:
+        # Using `width=<columns>`, `height=<lines>` and `preserveAspectRatio=0` ensures
+        # that an image always occupies the correct amount of columns and lines even if
+        # the cell size has changed when it's drawn.
+        # Since we use `width` and `height` control data keys, there's no need
+        # upscaling an image on this end; ensures minimal payload.
+
+        r_width, r_height = self.rendered_size
+        width, height = self._get_minimal_render_size()
+
+        img = self._get_render_data(img, alpha, size=(width, height), pixel_data=False)[
+            0
+        ]
+        format = "jpeg" if img.mode == "RGB" else "png"
+        if self._render_method == LINES:
+            raw_image = io.BytesIO(img.tobytes())
+            compressed_image = io.BytesIO()
+        else:
+            compressed_image = io.BytesIO()
+            img.save(compressed_image, format)
+
+        # clean up
+        if img is not self._source:
+            img.close()
+
+        # Workarounds
+        is_on_konsole = self._TERM == "konsole"
+        jump_right = f"\033[{r_width}C"
+
+        if self._render_method == LINES:
+            # NOTE: It's more efficient to write separate strings to the buffer
+            # separately than concatenate and write together.
+
+            cell_height = height // r_height
+            bytes_per_line = width * cell_height * (len(img.mode))
+            control_data = (
+                f";width={r_width};height=1;preserveAspectRatio=0;inline=1"
+                f"{';doNotMoveCursor=1' * is_on_konsole}:"
+            )
+
+            with io.StringIO() as buffer, raw_image, compressed_image:
+                for line in range(1, r_height + 1):
+                    compressed_image.seek(0)
+                    compressed_image.truncate()
+                    with PIL.Image.frombytes(
+                        img.mode, (width, cell_height), raw_image.read(bytes_per_line)
+                    ) as img:
+                        img.save(compressed_image, format)
+
+                    buffer.write(f"\033]1337;File=size={compressed_image.tell()}")
+                    buffer.write(control_data)
+                    buffer.write(
+                        standard_b64encode(compressed_image.getvalue()).decode()
+                    )
+                    buffer.write(ST)
+                    is_on_konsole and buffer.write(jump_right)
+                    line < r_height and buffer.write("\n")
+
+                return buffer.getvalue()
+        else:
+            with compressed_image:
+                control_data = "".join(
+                    (
+                        f"size={compressed_image.tell()};width={r_width}"
+                        f";height={r_height};preserveAspectRatio=0;inline=1"
+                        f"{';doNotMoveCursor=1' * is_on_konsole}:"
+                    )
+                )
+                return "".join(
+                    (
+                        "\033]1337;File=",
+                        control_data,
+                        standard_b64encode(compressed_image.getvalue()).decode(),
+                        ST,
+                        f"{jump_right}\n" * (r_height - 1) if is_on_konsole else "",
+                        jump_right * is_on_konsole,
+                    )
+                )
+
+
+ST = "\033\\"
