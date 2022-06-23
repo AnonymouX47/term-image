AnonymouX47/term-image

commit 112986ec2a3e4e6ecea5f88e81d5e35593a30a72

[lib,cli,tui] Implemented auto background color
- Fix: Corrected the description of disabled transparency.
- Add: Implemented the use of the terminal's default BG color for image BG color.
  - "#" is now a valid value for the *alpha* argument of `BaseImage.draw()`.
  - Incorporated changes into the CLI and TUI.
- Change: Modified `BaseImage._get_render_data()`.
- Change: Extended the common format specification.
- Change: Updated docstring of `BaseImage.draw()`.
- Change: Updated format specification docs.
- Change: Updated the description of `-b/--alpha-bg`.

term_image/tui/__init__.py
@@ -72,7 +72,13 @@ def init(
                 specs["kitty"] += f"c{style_args['compress']}"
 
     Image._ti_alpha = (
-        "#" if args.no_alpha else "#" + (args.alpha_bg or f"{args.alpha:f}"[1:])
+        "#"
+        if args.no_alpha
+        else (
+            f"#{args.alpha:f}"[1:]
+            if args.alpha_bg is None
+            else "#" + (args.alpha_bg or "#")
+        )
     )
     Image._ti_grid_style_spec = render.grid_style_specs.get(args.style, "")
 

term_image/cli.py
@@ -761,7 +761,7 @@ FOOTNOTES:
     alpha_options.add_argument(
         "--no-alpha",
         action="store_true",
-        help="Disable image transparency (i.e black background)",
+        help="Disable image transparency (alpha channel is removed)",
     )
     alpha_options.add_argument(
         "-A",
@@ -777,10 +777,12 @@ FOOTNOTES:
     alpha_options.add_argument(
         "-b",
         "--alpha-bg",
+        nargs="?",
+        const="",
         metavar="COLOR",
         help=(
-            "Hex color (without '#') with which transparent backgrounds should be "
-            "replaced"
+            "Hex color (without '#') to replace transparent backgrounds with "
+            "(omit `COLOR` to use the terminal's default BG color)"
         ),
     )
 
@@ -1406,7 +1408,9 @@ FOOTNOTES:
                     (
                         None
                         if args.no_alpha
-                        else args.alpha_bg and "#" + args.alpha_bg or args.alpha
+                        else (
+                            args.alpha if args.alpha_bg is None else "#" + args.alpha_bg
+                        )
                     ),
                     scroll=args.scroll,
                     animate=not args.no_anim,

term_image/image/common.py
@@ -44,11 +44,11 @@ from ..utils import (
 
 _ALPHA_THRESHOLD = 40 / 255  # Default alpha threshold
 _FORMAT_SPEC = re.compile(
-    r"(([<|>])?(\d+)?)?(\.([-^_])?(\d+)?)?(#(\.\d+|[0-9a-f]{6})?)?(\+(.+))?",
+    r"(([<|>])?(\d+)?)?(\.([-^_])?(\d+)?)?(#(\.\d+|[0-9a-f]{6}|#)?)?(\+(.+))?",
     re.ASCII,
 )
 _NO_VERTICAL_SPEC = re.compile(r"(([<|>])?(\d+)?)?\.(#(\.\d+|[0-9a-f]{6})?)?", re.ASCII)
-_HEX_COLOR_FORMAT = re.compile("#[0-9a-f]{6}", re.ASCII)
+_ALPHA_BG_FORMAT = re.compile("#([0-9a-f]{6})?", re.ASCII)
 
 
 @no_redecorate
@@ -540,13 +540,16 @@ class BaseImage(ABC):
 
             alpha: Transparency setting.
 
-              * If ``None``, transparency is disabled
-                (uses the image's default background color).
+              * If ``None``, transparency is disabled (alpha channel is removed).
               * If a ``float`` (**0.0 <= x < 1.0**), specifies the alpha ratio
                 **above** which pixels are taken as *opaque*. **(Applies to only
                 text-based render styles)**.
-              * If a string, specifies a **hex color** with which transparent
-                background should be replaced.
+              * If a string, specifies a color to replace transparent background with.
+                Can be:
+
+                * **"#"** -> The terminal's default background color (or black, if
+                  undetermined) is used.
+                * A hex color e.g ``ffffff``, ``7faa52``.
 
             scroll: Only applies to non-animations. If ``True``:
 
@@ -607,7 +610,7 @@ class BaseImage(ABC):
                 if not 0.0 <= alpha < 1.0:
                     raise ValueError(f"Alpha threshold out of range (got: {alpha})")
             elif isinstance(alpha, str):
-                if not _HEX_COLOR_FORMAT.fullmatch(alpha):
+                if not _ALPHA_BG_FORMAT.fullmatch(alpha):
                     raise ValueError(f"Invalid hex color string (got: {alpha})")
             else:
                 raise TypeError(
@@ -1074,8 +1077,8 @@ class BaseImage(ABC):
             (
                 threshold_or_bg
                 and (
-                    "#" + threshold_or_bg
-                    if _HEX_COLOR_FORMAT.fullmatch("#" + threshold_or_bg)
+                    "#" + threshold_or_bg.lstrip("#")
+                    if _ALPHA_BG_FORMAT.fullmatch("#" + threshold_or_bg.lstrip("#"))
                     else float(threshold_or_bg)
                 )
                 if alpha
@@ -1464,6 +1467,8 @@ class BaseImage(ABC):
         else:
             convert_resize_img("RGBA")
             if isinstance(alpha, str):
+                if alpha == "#":
+                    alpha = get_fg_bg_colors(hex=True)[1] or "#000000"
                 bg = Image.new("RGBA", img.size, alpha)
                 bg.alpha_composite(img)
                 if img is not self._source:

commit 1353a677bec37a91ad25304b0da920e2a1b6b3fe

[tui] Fixed TUI image format specifier
- Fix: Corrected alpha threshold field of the format specifier for the TUI.

Broke in 112986e.

term_image/tui/__init__.py
@@ -75,7 +75,7 @@ def init(
         "#"
         if args.no_alpha
         else (
-            f"#{args.alpha:f}"[1:]
+            "#" + f"{args.alpha:f}"[1:]
             if args.alpha_bg is None
             else "#" + (args.alpha_bg or "#")
         )