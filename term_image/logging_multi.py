83e0a10d523fb55891f4f11f1e232f45a22b46db
AnonymouX47 <anonymoux47@gmail.com>
2022-06-27 03:35:25 +0100

    [tui] Fixed `SWAP_WIN_SIZE` for the TUI
    - Fix: The value is now exported to subprocess when multiprocessing is enabled.

--- term_image/logging_multi.py
+++ term_image/logging_multi.py
@@ -60,6 +60,7 @@ class Process(Process):
         self._main_process_interrupted = cli.interrupted
         self._font_ratio = cli.args.font_ratio
         self._query_timeout = term_image.utils.QUERY_TIMEOUT
+        self._swap_win_size = term_image.utils.SWAP_WIN_SIZE
         self._ImageClass = tui.main.ImageClass
         exported_attrs = exported_style_attrs.get(cli.args.style)
         if self._ImageClass and exported_attrs:
@@ -76,6 +77,7 @@ class Process(Process):
 
         try:
             term_image.utils.QUERY_TIMEOUT = self._query_timeout
+            term_image.utils.SWAP_WIN_SIZE = self._swap_win_size
 
             if self._ImageClass:
                 # The unpickled class object is in the originally defined state
