616747ac921a75a615e5cebd9e93b625aa233e09
AnonymouX47 <anonymoux47@gmail.com>
2022-07-12 16:12:26 +0100

    [test] Set a fixed terminal size for all tests
    - Fix: Corrected some tests that failed within small terminal windows before.
    - Change: Fixed the terminal size for all tests to `(80, 30)`.

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,6 +12,12 @@ def set_cell_size(size):
     term_image._auto_font_ratio = None
 
 
+def get_terminal_size():
+    return (80, 30)
+
+
+term_image.utils.get_terminal_size = get_terminal_size
+
 cell_size = None
 term_image.get_cell_size = get_cell_size
 term_image.utils.get_cell_size = get_cell_size
