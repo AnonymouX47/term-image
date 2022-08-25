AnonymouX47/term-image

commit b10a5946b9c89027c00583333b53273345ad5e6f

[config,cli] Updated CL options of config options
- Change: Updated default value and validation of CL options with corresponding config options.
  - CL options with corresponding config options now have default values of `None`.
  - `None` will later be replaced with config option value.

src/term_image/cli.py
@@ -589,10 +589,12 @@ def main() -> None:
     if force_cli_mode:
         args.cli = True
 
+    # `check_arg()` requires logging.
     init_log(
         (
             args.log_file
-            if config_options["log file"].is_valid(args.log_file)
+            # If the argument is invalid, the error will be emitted later.
+            if args.log_file and config_options["log file"].is_valid(args.log_file)
             else config_options.log_file
         ),
         getattr(_logging, args.log_level),
@@ -621,12 +623,18 @@ def main() -> None:
 
     for name, option in config_options.items():
         var_name = name.replace(" ", "_")
-        value = getattr(args, var_name, None)
+        try:
+            arg_value = getattr(args, var_name)
         # Not all config options have corresponding command-line arguments
-        if value is not None and not option.is_valid(value):
+        except AttributeError:
+            continue
+
+        if arg_value is None:
+            setattr(args, var_name, option.value)
+        elif not option.is_valid(arg_value):
             arg_name = f"--{name.replace(' ', '-')}"
             notify.notify(
-                f"{arg_name}: {option.error_msg} (got: {value!r})",
+                f"{arg_name}: {option.error_msg} (got: {arg_value!r})",
                 level=notify.ERROR,
             )
             notify.notify(

src/term_image/parsers.py
@@ -98,7 +98,6 @@ general.add_argument(
     "--query-timeout",
     type=float,
     metavar="N",
-    default=config_options.query_timeout,
     help=(
         "Timeout (in seconds) for all terminal queries "
         f"(default: {config_options.query_timeout})"
@@ -108,7 +107,6 @@ general.add_argument(
     "-S",
     "--style",
     choices=("auto", "block", "iterm2", "kitty"),
-    default=config_options.style,
     help=(
         f"Image render style (default: {config_options.style}). "
         'See "Render Styles" below'
@@ -129,7 +127,6 @@ font_ratio_options.add_argument(
     "--font-ratio",
     type=float,
     metavar="N",
-    default=config_options.font_ratio,
     help=(
         "The width-to-height ratio of a character cell in the terminal, to "
         f"preserve image aspect ratio (default: {config_options.font_ratio or 'auto'})"
@@ -145,7 +142,6 @@ win_size_options = general.add_mutually_exclusive_group()
 win_size_options.add_argument(
     "--swap-win-size",
     action="store_true",
-    default=config_options.swap_win_size,
     help=(
         "A workaround for 'auto font ratio' on some terminal emulators (e.g older "
         "VTE-based ones) that wrongly report window dimensions swapped"
@@ -154,7 +150,6 @@ win_size_options.add_argument(
 win_size_options.add_argument(
     "--no-swap-win-size",
     action="store_false",
-    default=config_options.swap_win_size,
     dest="swap_win_size",
     help="Unlike '--swap-win-size', use the reported window size as-is (default)",
 )
@@ -202,7 +197,6 @@ anim_cache_options = anim_options.add_mutually_exclusive_group()
 anim_cache_options.add_argument(
     "--anim-cache",
     type=int,
-    default=config_options.anim_cache,
     metavar="N",
     help=(
         "Maximum frame count for animation frames to be cached (Better performance "
@@ -460,7 +454,6 @@ perf_options.add_argument(
     "--max-pixels",
     type=int,
     metavar="N",
-    default=config_options.max_pixels,
     help=(
         "Maximum amount of pixels in images to be displayed "
         f"(default: {config_options.max_pixels}) [4]"
@@ -471,7 +464,6 @@ perf_options.add_argument(
     "--checkers",
     type=int,
     metavar="N",
-    default=config_options.checkers,
     help=(
         "Maximum number of sub-processes for checking directory sources "
         f"(default: {config_options.checkers})"
@@ -481,7 +473,6 @@ perf_options.add_argument(
     "--getters",
     type=int,
     metavar="N",
-    default=config_options.getters,
     help=(
         "Number of threads for downloading images from URL sources "
         f"(default: {config_options.getters})"
@@ -491,7 +482,6 @@ perf_options.add_argument(
     "--grid-renderers",
     type=int,
     metavar="N",
-    default=config_options.grid_renderers,
     help=(
         "Number of subprocesses for rendering grid cells "
         f"(default: {config_options.grid_renderers})"
@@ -502,14 +492,12 @@ multi_options = perf_options.add_mutually_exclusive_group()
 multi_options.add_argument(
     "--multi",
     action="store_false",
-    default=config_options.no_multi,
     dest="no_multi",
     help="Enable multiprocessing, if supported (default)",
 )
 multi_options.add_argument(
     "--no-multi",
     action="store_true",
-    default=config_options.no_multi,
     help="Disable multiprocessing",
 )
 
@@ -524,7 +512,6 @@ log_options_.add_argument(
     "-l",
     "--log-file",
     metavar="FILE",
-    default=config_options.log_file,
     help=f"The file to write logs to (default: {config_options.log_file})",
 )
 log_options.add_argument(

commit faa5bad1b089df1a2aacae1cbd73a00f893e51a3

[config,cli] Fixed and updated config/CL options
- Fix: Changed default value of CL options corresponding to **boolean** config options to `None`.
  - `--multi` & `--no-multi`
  - `--swap-win-size` & `--no-swap-win-size`
  - In order for their values to be overriden by the corresponding config value when not specified.
  - Was Broken in commit b10a5946b9c89027c00583333b53273345ad5e6f.
- Change: Deferred computation of auto value for "checkers" till after `--checkers` has been processed.
- Change: Updated some CL option descriptions.

src/term_image/cli.py
@@ -599,7 +599,7 @@ def main() -> None:
         ),
         getattr(_logging, args.log_level),
         args.debug,
-        args.no_multi,
+        config_options.no_multi if args.no_multi is None else args.no_multi,
         args.quiet,
         args.verbose,
         args.verbose_log,
@@ -748,6 +748,16 @@ def main() -> None:
     opener_started = False
 
     if OS_IS_UNIX and not args.cli:
+        if args.checkers is None:
+            args.checkers = max(
+                (
+                    len(os.sched_getaffinity(0))
+                    if hasattr(os, "sched_getaffinity")
+                    else os.cpu_count() or 0
+                )
+                - 1,
+                2,
+            )
         dir_queue = mp_Queue() if logging.MULTI and args.checkers > 1 else Queue()
         dir_queue.sources_finished = False
         check_manager = Thread(

src/term_image/config.py
@@ -99,17 +99,6 @@ def init_config() -> None:
     context_keys["global"]["Config"][3] = False  # Till the config menu is implemented
     expand_key[3] = False  # "Key bar" action should be hidden
 
-    if config_options.checkers is None:
-        config_options.checkers = max(
-            (
-                len(os.sched_getaffinity(0))
-                if hasattr(os, "sched_getaffinity")
-                else os.cpu_count() or 0
-            )
-            - 1,
-            2,
-        )
-
 
 def load_config(config_file: str) -> None:
     """Loads a user config file."""

src/term_image/parsers.py
@@ -129,19 +129,20 @@ font_ratio_options.add_argument(
     metavar="N",
     help=(
         "The width-to-height ratio of a character cell in the terminal, to "
-        f"preserve image aspect ratio (default: {config_options.font_ratio or 'auto'})"
+        "preserve image aspect ratio (default: auto)"
     ),
 )
 font_ratio_options.add_argument(
     "--auto-font-ratio",
     action="store_true",
-    help="Determine the font ratio from the terminal, if possible",
+    help="Determine the font ratio from the terminal, if possible (default)",
 )
 
 win_size_options = general.add_mutually_exclusive_group()
 win_size_options.add_argument(
     "--swap-win-size",
     action="store_true",
+    default=None,
     help=(
         "A workaround for 'auto font ratio' on some terminal emulators (e.g older "
         "VTE-based ones) that wrongly report window dimensions swapped"
@@ -150,6 +151,7 @@ win_size_options.add_argument(
 win_size_options.add_argument(
     "--no-swap-win-size",
     action="store_false",
+    default=None,
     dest="swap_win_size",
     help="Unlike '--swap-win-size', use the reported window size as-is (default)",
 )
@@ -465,8 +467,7 @@ perf_options.add_argument(
     type=int,
     metavar="N",
     help=(
-        "Maximum number of sub-processes for checking directory sources "
-        f"(default: {config_options.checkers})"
+        "Maximum number of sub-processes for checking directory sources (default: auto)"
     ),
 )
 perf_options.add_argument(
@@ -492,12 +493,14 @@ multi_options = perf_options.add_mutually_exclusive_group()
 multi_options.add_argument(
     "--multi",
     action="store_false",
+    default=None,
     dest="no_multi",
     help="Enable multiprocessing, if supported (default)",
 )
 multi_options.add_argument(
     "--no-multi",
     action="store_true",
+    default=None,
     help="Disable multiprocessing",
 )
 