AnonymouX47/term-image

commit ea88da04d2f22a5fd3671f23dca59bb3d68d5f0d

[config] Implemented support for XDG Base Dirs
- Add: `$XDG_CONFIG_HOME` and `$XDG_CONFIG_DIRS` are now searched for user config files.
- Add: Added `load_xdg_config()`, `xdg_config_file` and `user_config_file` in `.config`.
- Change: Config files are no longer written into or searched for in `$HOME/.term_image/`.

Refs: #63

src/term_image/config.py
@@ -6,6 +6,7 @@ import json
 import os
 import sys
 from copy import deepcopy
+from os import path
 from typing import Dict
 
 import urwid
@@ -39,7 +40,7 @@ def fatal(msg: str) -> None:
 
 
 def init_config() -> None:
-    """Initializes user configuration
+    """Initializes user configuration.
 
     IMPORTANT:
         Must be called before any other function in this module
@@ -47,25 +48,15 @@ def init_config() -> None:
     """
     global checkers
 
-    if os.path.exists(user_dir):
-        if not os.path.isdir(user_dir):
-            fatal(f"Please rename or remove the file {user_dir!r}.")
-            sys.exit(CONFIG_ERROR)
-    else:
-        os.mkdir(user_dir)
-
-    if os.path.isfile(config_file):
-        if load_config():
-            # Stored at this point in order to put missing values in place.
-            store_config()
-            info("... Successfully updated user config.")
-    else:
-        update_context_nav_keys(context_keys, nav, nav)
-        store_config(default=True)
+    update_context_nav_keys(context_keys, nav, nav)  # Update symbols
+    if user_config_file:
+        load_config(user_config_file)
+    elif xdg_config_file:
+        load_xdg_config()
 
     for keyset in context_keys.values():
         for action in keyset.values():
-            action[3:] = (True, True)  # Default: "shown", "enabled"
+            action[3:] = (True, True)  # "shown", "enabled"
     context_keys["global"]["Config"][3] = False  # Till the config menu is implemented
     expand_key[3] = False  # "Key bar" action should be hidden
 
@@ -129,6 +120,21 @@ def load_config(config_file: str) -> None:
                 update_context(context, context_keys[context], keyset, config_file)
             else:
                 error(f"Unknown context {context!r}.")
+
+
+def load_xdg_config() -> None:
+    """Loads user config files according to the XDG Base Directories spec."""
+    for config_dir in reversed(os.environ.get("XDG_CONFIG_DIRS", "/etc").split(":")):
+        config_file = path.join(config_dir, "term_image", "config.json")
+        if (
+            # The XDG Base Dirs spec states that relative paths should be ignored
+            path.abspath(config_dir) == config_dir
+            and path.isfile(config_file)
+        ):
+            load_config(config_file)
+
+    if path.isfile(xdg_config_file):
+        load_config(xdg_config_file)
 
 
 def store_config(*, default: bool = False) -> None:
@@ -257,8 +263,13 @@ def update_context_nav_keys(
                 properties[:2] = nav_update[navi[properties[0]]]
 
 
-user_dir = os.path.join(os.path.expanduser("~"), ".term_image")
-config_file = os.path.join(user_dir, "config.json")
+user_dir = path.join(path.expanduser("~"), ".term_image")
+user_config_file = None
+xdg_config_file = path.join(
+    os.environ.get("XDG_CONFIG_HOME", path.join(path.expanduser("~"), ".config")),
+    "term_image",
+    "config.json",
+)
 
 _valid_keys = {*bytes(range(32, 127)).decode(), *urwid.escape._keyconv.values(), "esc"}
 _valid_keys.update(
@@ -311,7 +322,7 @@ checkers = _checkers = None
 font_ratio = _font_ratio = None
 getters = _getters = 4
 grid_renderers = _grid_renderers = 1
-log_file = _log_file = os.path.join(user_dir, "term_image.log")
+log_file = _log_file = path.join(user_dir, "term_image.log")
 max_notifications = _max_notifications = 2
 max_pixels = _max_pixels = 2**22  # 2048x2048
 no_multi = _no_multi = False
@@ -446,9 +457,9 @@ config_options = {
             isinstance(x, str)
             and (
                 # exists, is a file and writable
-                (os.path.isfile(x) and os.access(x, os.W_OK))
+                (path.isfile(x) and os.access(x, os.W_OK))
                 # is not a directory and the parent directory is writable
-                or (not os.path.isdir(x) and os.access(os.path.dirname(x), os.W_OK))
+                or (not path.isdir(x) and os.access(path.dirname(x), os.W_OK))
             )
         ),
         "must be a string containing a writable path to a file",

commit 48bc4281edb60f45725194e06df5a26d5037f69d

[config] Fixed and updated "log file" validation
- Fix: Validation now fails for an unwritable file in writable directory.
- Add: Added tilde expansion support.
- Add: Added `.utils.is_writable()`.
- Change: Now accepts non-existent paths that require creating [multiple] parent directories.
  - Implicitly fixes creation of default log file for the first time i.e when the parent diretory
    does not exist, was broken in ea88da04d2f22a5fd3671f23dca59bb3d68d5f0d.

src/term_image/config.py
@@ -13,7 +13,7 @@ from typing import Any, Callable, Dict, Optional, Tuple
 import urwid
 
 from .exit_codes import CONFIG_ERROR
-from .utils import COLOR_RESET, CSI, QUERY_TIMEOUT
+from .utils import COLOR_RESET, CSI, QUERY_TIMEOUT, is_writable
 
 
 class ConfigOptions(dict):
@@ -382,7 +382,6 @@ def update_context_nav(
                 properties[:2] = nav_update[context_nav[action]]
 
 
-user_dir = path.join(path.expanduser("~"), ".term_image")
 user_config_file = None
 xdg_config_file = path.join(
     os.environ.get("XDG_CONFIG_HOME", path.join(path.expanduser("~"), ".config")),
@@ -466,17 +465,9 @@ config_options = {
         "must be a non-negative integer",
     ),
     "log file": Option(
-        path.join(user_dir, "term_image.log"),
-        lambda x: (
-            isinstance(x, str)
-            and (
-                # exists, is a file and writable
-                (path.isfile(x) and os.access(x, os.W_OK))
-                # is not a directory and the parent directory is writable
-                or (not path.isdir(x) and os.access(path.dirname(x) or ".", os.W_OK))
-            )
-        ),
-        "must be a string containing a writable file path",
+        path.join("~", ".term_image", "term_image.log"),
+        lambda x: isinstance(x, str) and is_writable(x),
+        "must be a string containing a writable/creatable file path",
     ),
     "max notifications": Option(
         2,

src/term_image/logging.py
@@ -27,6 +27,8 @@ def init_log(
     """Initialize application event logging"""
     global DEBUG, MULTI, QUIET, VERBOSE, VERBOSE_LOG
 
+    logfile = os.path.expanduser(logfile)
+    os.makedirs(os.path.dirname(logfile) or ".", exist_ok=True)
     handler = RotatingFileHandler(
         logfile,
         maxBytes=2**20,  # 1 MiB

src/term_image/utils.py
@@ -31,6 +31,7 @@ from array import array
 from functools import wraps
 from multiprocessing import Array, Process, Queue as mp_Queue, RLock as mp_RLock
 from operator import floordiv
+from pathlib import Path
 from queue import Empty, Queue
 from shutil import get_terminal_size as _get_terminal_size
 from threading import RLock
@@ -420,6 +421,34 @@ def get_window_size() -> Optional[Tuple[int, int]]:
         size = size[:: -SWAP_WIN_SIZE or 1] if size else (0, 0)
         _win_size_cache[:] = ts + size
         return None if 0 in size else size
+
+
+def is_writable(path: Union[str, os.PathLike, Path]) -> bool:
+    """Checks if a file path is writable or creatable.
+
+    Returns:
+      - ``True``, if:
+        - the file exists and is writable
+        - the file doesn't exists but can be created
+      - ``False``, if:
+        - the path points to a directory
+        - the file exists but is unwritable
+        - the file doesn't exists and cannot be created
+    """
+    path = Path(path).expanduser()
+    writable = False
+
+    if path.exists():
+        if path.is_file() and os.access(path, os.W_OK):
+            writable = True
+    else:
+        for path in path.parents:
+            if path.exists():
+                if path.is_dir() and os.access(path, os.W_OK):
+                    writable = True
+                break
+
+    return writable
 
 
 @unix_tty_only