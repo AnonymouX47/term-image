21fe341fb70858a7a4b8df18825276591bf09ca4
AnonymouX47 <anonymoux47@gmail.com>
2023-04-16 15:47:06 +0100

    docs: Cleanup docs config
    - Change: Use `html_logo` instead of `html_theme_options`
      (+ `html_static_path`).
    - Change: Move `toc_object_entries` to "General configuration".
    - Change: Remove unneeded and unused options.
    - Change: Update comments.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of configuration options, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 from term_image import __version__, utils
@@ -14,18 +13,12 @@ from term_image import __version__, utils
 # sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
-
 project = "Term-Image"
 copyright = "2022, Toluwaleke Ogundipe"
 author = "Toluwaleke Ogundipe"
 release = __version__
 
-
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
@@ -35,40 +28,12 @@ extensions = [
     "sphinx_toolbox.sidebar_links",
     "sphinx_toolbox.more_autosummary",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = []
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+toc_object_entries = False
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 html_theme = "furo"
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    "light_logo": "logo.png",
-    "dark_logo": "logo.png",
-}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["resources"]
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
+html_logo = "resources/logo.png"
 html_favicon = "resources/logo.ico"
-
 
 # -- Options for extensions ----------------------------------------------
 
@@ -100,11 +65,6 @@ github_repository = "term-image"
 
 # # -- sphinx_toolbox-more_autosummary ----------------------------------------------
 autodocsumm_member_order = "bysource"
-
-
-# -- Others options ----------------------------------------------------------
-toc_object_entries = False
-
 
 # -- Event handlers -------------------------------------------------------------
 
