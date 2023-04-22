dba9b65f352caa830297e63c8ae855ae9dce67f3
AnonymouX47 <anonymoux47@gmail.com>
2023-04-22 04:56:03 +0100

    chore: Bump package version to `0.6.1-dev`

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="term-image",
-    version="0.6.0",
+    version="0.6.1-dev",
     author="Toluwaleke Ogundipe",
     author_email="anonymoux47@gmail.com",
     url="https://github.com/AnonymouX47/term-image",
