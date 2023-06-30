6a549d9705fbcadc807451324b2eecdc086fbc73
AnonymouX47 <anonymoux47@gmail.com>
2023-06-30 14:46:40 +0100

    refactor: `get_cell_size() -> .geometry.Size`
    - Change: `.utils.get_cell_size()` now returns `.geometry.Size`
      instances in place of tuples.

--- src/term_image/utils.py
+++ src/term_image/utils.py
@@ -27,7 +27,7 @@ from time import monotonic
 from types import FunctionType
 from typing import Any, Callable, Optional, Tuple, Union
 
-from . import ctlseqs
+from . import ctlseqs, geometry
 from .exceptions import TermImageWarning
 
 # import logging
@@ -341,7 +341,7 @@ def color(
 
 
 @unix_tty_only
-def get_cell_size() -> Optional[Tuple[int, int]]:
+def get_cell_size() -> geometry.Size | None:
     """Returns the current size of a character cell in the :term:`active terminal`.
 
     Returns:
@@ -361,7 +361,7 @@ def get_cell_size() -> Optional[Tuple[int, int]]:
         ts = get_terminal_size()
         if ts == tuple(_cell_size_cache[:2]):
             size = tuple(_cell_size_cache[2:])
-            return None if 0 in size else size
+            return None if 0 in size else geometry.Size(*size)
 
         size = text_area_size = None
 
@@ -408,7 +408,7 @@ def get_cell_size() -> Optional[Tuple[int, int]]:
         )
         _cell_size_cache[:] = ts + size
 
-        return None if 0 in size else size
+        return None if 0 in size else geometry.Size(*size)
 
 
 @cached
