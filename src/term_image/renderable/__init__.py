65fc55130a41055a5bdb6706f8914a88e5cc1ac9
AnonymouX47 <anonymoux47@gmail.com>
2023-07-18 01:54:27 +0100

    feat: Export definitions of the Renderable API
    - Add: Export public definitions from `.renderable`:
      - `FrameCount`
      - `FrameDuration`
      - `HAlign`
      - `VAlign`
      - `Renderable`
      - `RenderArgs`
      - `RenderData`
      - `RenderFormat`
      - `RenderParam`
      - `Frame`

--- src/term_image/renderable/__init__.py
+++ src/term_image/renderable/__init__.py
@@ -3,3 +3,20 @@
 """
 
 from __future__ import annotations
+
+__all__ = (
+    "FrameCount",
+    "FrameDuration",
+    "HAlign",
+    "VAlign",
+    "Renderable",
+    "RenderArgs",
+    "RenderData",
+    "RenderFormat",
+    "RenderParam",
+    "Frame",
+)
+
+from ._enum import FrameCount, FrameDuration, HAlign, VAlign
+from ._renderable import Renderable
+from ._types import Frame, RenderArgs, RenderData, RenderFormat, RenderParam
