52c6419f6fedb058066ac9d9f4e5138b27c616dd
AnonymouX47 <anonymoux47@gmail.com>
2023-07-18 01:41:46 +0100

    feat: Add exception classes for the Renderable API
    - Add: Add new exception classes for the Renderable API.
      - `RenderableError`
      - `RenderArgsError`
      - `RenderDataError`
      - `RenderFormatError`
    - Change: `RenderError` is now a subclass of `RenderableError`.
    - Change: Generalize the description of `InvalidSizeError`.

--- src/term_image/exceptions.py
+++ src/term_image/exceptions.py
@@ -14,15 +14,35 @@ class TermImageError(Exception):
 
 
 class InvalidSizeError(TermImageError):
-    """Raised for invalid image sizes."""
+    """Raised for invalid sizes."""
 
 
-class RenderError(TermImageError):
+class RenderableError(TermImageError):
+    """Base exception for errors specific to the :py:mod:`Renderable API
+    <term_image.renderable>` and other APIs extending it.
+
+    Raised for errors specific to :py:class:`~term_image.renderable.Renderable`.
+    """
+
+
+class RenderArgsError(RenderableError):
+    """Raised for errors specific to :py:class:`~term_image.renderable.RenderArgs`."""
+
+
+class RenderDataError(RenderableError):
+    """Raised for errors specific to :py:class:`~term_image.renderable.RenderData`."""
+
+
+class RenderError(RenderableError):
     """Raised for errors that occur **during** :term:`rendering`.
 
     If the direct cause of the error is an exception, it should typically be attached
     as the context of this exception i.e ``raise RenderError(...) from exc``.
     """
+
+
+class RenderFormatError(TermImageError):
+    """Raised for errors specific to :py:class:`~term_image.renderable.RenderFormat`."""
 
 
 class StyleError(TermImageError):
