45c12dba429a528b09492d2ccc6a32ef017afae4
AnonymouX47 <anonymoux47@gmail.com>
2023-09-06 03:13:16 +0100

    docs: Add `prettyspecialmethods` sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,8 @@
 # For the full list of configuration options, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from sphinxcontrib import prettyspecialmethods
+
 from term_image import __version__, utils
 from term_image.image.common import ImageMeta
 from term_image.image.iterm2 import ITerm2ImageMeta
@@ -30,6 +32,7 @@ extensions = [
     "sphinx_toolbox.sidebar_links",
     "sphinx_toolbox.more_autosummary",
     "sphinx_toolbox.collapse",
+    "sphinxcontrib.prettyspecialmethods",
 ]
 
 # -- Options for HTML output -------------------------------------------------
@@ -68,6 +71,8 @@ github_repository = "term-image"
 autodocsumm_member_order = "bysource"
 
 # -- Event handlers -------------------------------------------------------------
+
+# # -- autodocsumm -----------------------------------------------------------
 
 
 def autodocssumm_grouper(app, what, name, obj, section, parent):
@@ -119,6 +124,9 @@ def autodocssumm_grouper(app, what, name, obj, section, parent):
             return "Special Attributes"
 
 
+# # -- Setup Function ------------------------------------------------------------------
+
+
 def setup(app):
     app.connect("autodocsumm-grouper", autodocssumm_grouper)
 
@@ -131,3 +139,30 @@ for meta in (ImageMeta, ITerm2ImageMeta):
     for attr, value in tuple(vars(meta).items()):
         if isinstance(value, utils.ClassPropertyBase):
             delattr(meta, attr)
+
+# # -- prettyspecialmethods ------------------------------------------------------
+
+
+def reflected_binary_op_transformer(op):
+    from sphinxcontrib.prettyspecialmethods import Text, emphasis, inline, patch_node
+
+    def xf(name_node, parameters_node):
+        return inline(
+            "",
+            "",
+            emphasis("", parameters_node.children[0].astext()),
+            Text(" "),
+            patch_node(name_node, op, ()),
+            Text(" "),
+            emphasis("", "self"),
+        )
+
+    return xf
+
+
+def skip_undoc_special_methods(*args, **kwargs):
+    pass
+
+
+prettyspecialmethods.SPECIAL_METHODS["__ror__"] = reflected_binary_op_transformer("|")
+prettyspecialmethods.show_special_methods = skip_undoc_special_methods
