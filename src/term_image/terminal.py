4bf30e6d48ced894a3d7d844af17edd24996c8fa
AnonymouX47 <anonymoux47@gmail.com>
2024-09-10 16:12:03 +0100

    feat:terminal: Add active terminal derivation
    - Add: `get_active_terminal()`.

--- src/term_image/terminal.py
+++ src/term_image/terminal.py
@@ -4,12 +4,24 @@
 
 from __future__ import annotations
 
-__all__ = ("TTY",)
+__all__ = ("TTY", "get_active_terminal")
 
+import os
+import sys
 from collections.abc import Callable
 from io import FileIO
 
 from .utils import arg_value_error_msg
+
+OS_IS_UNIX: bool
+try:
+    import fcntl  # noqa: F401
+    import termios  # noqa: F401
+    from select import select  # noqa: F401
+except ImportError:
+    OS_IS_UNIX = False
+else:
+    OS_IS_UNIX = True
 
 
 class TTY(FileIO):
@@ -39,3 +51,44 @@ class TTY(FileIO):
             raise arg_value_error_msg(
                 "'fd_or_name' is not [connected to] a TTY[-like] device", fd_or_name
             )
+
+
+def get_active_terminal() -> TTY | None:
+    """Determines the :term:`active terminal`.
+
+    Returns:
+        - the :py:class:`TTY` instance associated with the :term:`active terminal`, OR
+        - ``None``, on non-unix-like platforms or when there is no
+          :term:`active terminal`.
+    """
+    global _tty, _tty_determined
+
+    if _tty_determined:
+        return _tty
+
+    _tty_determined = True
+
+    if not OS_IS_UNIX:
+        return None
+
+    for stream in ("out", "in", "err"):  # In order of priority
+        try:
+            # A new file descriptor is required because both read and write access
+            # are required and in case the file descriptor is accidental closed.
+            tty_name = os.ttyname(getattr(sys, f"__std{stream}__").fileno())
+            break
+        except (OSError, AttributeError):
+            pass
+    else:
+        tty_name = "/dev/tty"
+
+    try:
+        _tty = TTY(tty_name)
+    except OSError:
+        return None
+
+    return _tty
+
+
+_tty: TTY | None = None
+_tty_determined: bool = False
